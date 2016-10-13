33139c2c3ab26f2a51603585a0ade846a9f6862e
Adam Szakal <adam.b.szakal@gmail.com>
2016-10-13 17:06:59 +0200

    fix av fuckup

--- app/controllers/TrixController.js
+++ app/controllers/TrixController.js
@@ -18,7 +18,7 @@
             var noTrix = Math.floor((Math.random() * 4) + 2);
             var trixString = '';
             for (var i=0; i < noTrix;i++){
-                trixString += getRandomTrick() + ' <span style="opacity: .3"> + </span';
+                trixString += getRandomTrick() + ' <span style="opacity: .3;"> + </span';
             }
             trixString += getRandomTrick();
             $scope.line = trixString;
