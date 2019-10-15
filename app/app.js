7613f527f90a9a8c0ddb0f367e74f4ba3008c23b
Christophe van Baalen <chichoch@gmail.com>
2019-10-15 09:50:23 +0200

    Fille

--- app/app.js
+++ app/app.js
@@ -7,6 +7,10 @@
 				controller: 'TrixController',
 				templateUrl: 'app/views/trix.html'
 			})
+			.when('/tåhäv', {
+				controller: 'TrixController',
+				templateUrl: 'app/views/fille.html'
+			})
 			.otherwise( { redirectTo: '/'});
 	});
 
